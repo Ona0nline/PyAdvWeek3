86e297503ab67fef1860a88ab134e6cd4cdb1158
Ona0nline <onasihle123@gmail.com>
2024-09-04 12:54:06 +0200

    Update main.py

--- Lesson 5/main.py
+++ Lesson 5/main.py
@@ -3,12 +3,19 @@ import matplotlib.pyplot as plt
 condition = ['sunny', 'rainy', 'cloudy']
 days = [300, 30, 35]
 
-fig,(ax1,ax2) = plt.subplots(2,1)
+fig,(ax1,ax2) = plt.subplots(1,2)
 
 ax1.plot(condition,days)
 ax2.plot(condition,days)
 
-plt.pie(days, labels=condition)
-plt.title('Weather in Lisbon')
-plt.legend(days)
+ax1.pie(days, labels=condition)
+ax1.set_title('Weather in Lisbon')
+ax1.legend(days)
+
+
+ax2.pie(days, labels=condition)
+ax2.set_title('Weather in Lisbon')
+ax2.legend(days)
+
+plt.tight_layout()
 plt.show()
